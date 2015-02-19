695b16c8305ac76011dd3e927ffd38df5b2b05c7
taa1963 <taa1963@ukr.net>
2015-02-19 23:22:48 +0200

    2002

---
+++ test9.ru/js/init.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+	$('.slider').mobilyslider({
+	  autoplay: true,
+	  pauseOnHover: true,
+	  autoplaySpeed: 4000,
+	  transition: 'fade',
+	  animationSpeed: 800,
+	  //bullets: true,
+	  //arrowsHide: true
+	});
+	 
+});

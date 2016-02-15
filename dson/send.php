b21131ffa2fb92cfac6c6c0962a9c133de68ff85
taa1963 <taa1963@ukr.net>
2016-02-15 23:36:32 +0200

    verstka15022016_1

--- dson/send.php
+++ dson/send.php
@@ -28,39 +28,12 @@ if (
  $verify = mail($name, $email, "Content-type:text/plain; Charset=windows-1251\r\n");
 
  if ($verify == 'true')
- {
-  echo "
-<link rel='stylesheet' type='text/css' href='style-send.css'/>
-<table width='600' height='300' align='center'>
- <tr>
- <td class='warning_table' width='220' align='center' valign='middle'>
 
- <img align='center' src='img/screens.png'>
- <div align='center' class='warning_font_big'>Поздравляем!</div>
- <div align='center' class='warning_font' align='left'>Ваше Ваша заявка доставлена администратору. Через некоторое время Вы получите ответ!</div>
- <p align='center'><a href='index.html' class='all_links'>Вернуться назад</a></div></p>
 
- </td>
- </tr>
- </table>";
- }
- else
- {
-  echo "
-<link rel='stylesheet' type='text/css' href='style-send.css'/>
-<table width='600' height='300' align='center'>
- <tr>
- <td class='warning_table' width='220' align='center' valign='middle'>
-
- <img align='center' src='img/warning.png'>
- <div align='center' class='warning_font_big'>ОШИБКА!!!</div>
- <div align='center' class='warning_font' align='left'>Ваше письмо не доставлено. Повторите отправку немного позже!</div>
- <p align='center'><a href='index.html' class='all_links'>Вернуться назад</a></div></p>
-
- </td>
- </tr>
- </table>";
- }
+   {echo 'Сообщение успешно отправлено';}
 }
+else {echo 'Ошибка отправки сообщения';
+}
+
 ?>
 

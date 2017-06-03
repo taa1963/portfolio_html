ecead7b45db21402e19c9a201a413cabe5936d6b
taa1963 <taa1963@ukr.net>
2017-06-04 00:51:59 +0300

    04/06/2017_1

--- NewProperty/js/slider.js
+++ NewProperty/js/slider.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-	function htmSlider(){
+	function htmSlider() {
 		/* Зададим следующие переменные */
 
 		/* обертка слайдера */
@@ -9,20 +9,20 @@ jQuery(document).ready(function(){
 		var prevLink = jQuery('.prev-slide');
 
 		var playLink = jQuery('.auto');
-		
+
 		var is_animate = true;
-		
+
 		/* ширина слайда с отступами */
 		var slideWidth = jQuery('.slide-item').outerWidth();
-		
+
 		/* смещение слайдера */
 		var newLeftPos = slideWrap.position().left - slideWidth;
-		
+
 		/* Клик по ссылке на следующий слайд */
-		nextLink.click(function(){
-			if(!slideWrap.is(':animated')) {
-	
-				slideWrap.animate({left: newLeftPos}, 500, function(){
+		nextLink.click(function () {
+			if (!slideWrap.is(':animated')) {
+
+				slideWrap.animate({left: newLeftPos}, 500, function () {
 					slideWrap
 						.find('.slide-item:first')
 						.appendTo(slideWrap)
@@ -34,9 +34,9 @@ jQuery(document).ready(function(){
 		});
 
 		/* Клик по ссылке на предыдующий слайд */
-		prevLink.click(function(){
-			if(!slideWrap.is(':animated')) {
-			
+		prevLink.click(function () {
+			if (!slideWrap.is(':animated')) {
+
 				slideWrap
 					.css({'left': newLeftPos})
 					.find('.slide-item:last')
@@ -46,38 +46,121 @@ jQuery(document).ready(function(){
 
 			}
 		});
-		
-		
-		/* Функция автоматической прокрутки слайдера */
-		function autoplay(){
-			if(!is_animate){
-				is_animate = true;
-				slideWrap.animate({left: newLeftPos}, 500, function(){
-					slideWrap
-						.find('.slide-item:first')
-						.appendTo(slideWrap)
-						.parent()
-						.css({'left': 0});
-					is_animate = false;
-				});
-			}
-		}
-		
-		/* Клики по ссылкам старт/пауза */
-		playLink.click(function(){
-			if(playLink.hasClass('play')){
-				playLink.removeClass('play').addClass('pause');
-				jQuery('.navy').addClass('disable');
-				timer = setInterval(autoplay, 1000);
-			} else {
-				playLink.removeClass('pause').addClass('play');
-				jQuery('.navy').removeClass('disable');
-				clearInterval(timer);
-			}
-		});
 
+
+		/* Функция автоматической прокрутки слайдера */
+		/*function autoplay(){
+		 if(!is_animate) {
+			 is_animate = true;
+			 slideWrap.animate({left: newLeftPos}, 500, function () {
+				 slideWrap
+					 .find('.slide-item:first')
+					 .appendTo(slideWrap)
+					 .parent()
+					 .css({'left': 0});
+				 is_animate = false;
+			 }
+		 }
+
+		 });*/
+
+
+		 /*function autoplay  () {
+			 if (!slideWrap.is(':animated')) {
+
+				 slideWrap
+					 .css({'left': newLeftPos})
+					 .find('.slide-item:last')
+					 .prependTo(slideWrap)
+					 .parent()
+					 .animate({left: 0}, 500);
+			 }
+
+
+		});*/
+
+
+
+/*
+		function autoplay() {
+
+			var timerId = setInterval(function () {
+
+		/!* alert(eee);*!/
+				if (!slideWrap.is(':animated')) {
+
+					slideWrap
+						.css({'left': newLeftPos})
+						.find('.slide-item:last')
+						.prependTo(slideWrap)
+						.parent()
+						.animate({left: 0}, 500);
+
+				}
+
+		/!* slideWrap
+		 .css({'left': newLeftPos})
+		 .find('.slide-item:last')
+		 .prependTo(slideWrap)
+		 .parent()
+		 .animate({left: 0}, 500);*!/
+
+
+		 }, 500);
+			alert(eee);
+
+		 };*/
+
+
+		/*	is_animate = true;
+		 slideWrap.animate({left: newLeftPos}, 2000, function () {
+
+		 slideWrap
+		 .find('.slide-item:first')
+		 .appendTo(slideWrap)
+		 .parent()
+		 .css({'left': 0});
+		 is_animate = false;
+		 })*/
+
+		/* };*/
+
+
+		/*function autoplay(){
+		 if(!is_animate){
+		 is_animate = true;
+		 slideWrap.animate({left: newLeftPos}, 500, function(){
+		 slideWrap
+		 .find('.slide-item:first')
+		 .appendTo(slideWrap)
+		 .parent()
+		 .css({'left': 0});
+		 is_animate = false;
+		 });
+		 }
+		 }
+
+		 Клики по ссылкам старт/пауза
+		 playLink.click(function(){
+
+		 if(playLink.hasClass('play')){
+		 playLink.removeClass('play').addClass('pause');
+		 jQuery('.navy').addClass('disable');
+		 timer = setInterval(autoplay, 1000);
+		 }
+
+
+		 });
+
+		 }*/
 	}
 
 	/* иницилизируем функцию слайдера */
 	htmSlider();
-	});
+	/*autoplay();*/
+	/*var timerId = setInterval(function() {
+		alert(eee) ;
+	}, 2000);*/
+
+
+});

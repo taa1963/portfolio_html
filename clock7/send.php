8d3d319733389250b7029ec23beb59c4c2c874d6
taa1963 <taa1963@ukr.net>
2019-03-22 22:38:08 +0200

    t22_03_2019

--- clock7/send.php
+++ clock7/send.php
@@ -22,7 +22,7 @@ if (
 
     $verify = mail($to, $title, $message, "Content-type:text/plain; Charset=utf-8\r\n");
 
-//    echo "<center>Ваше сообщениеуспешно <br><center> отправлено<BR><center><a href='http://www.partwork.ru/angrybirds/'>Вернуться на сайт</a>";
+   echo "<center>Заявка отправлена<BR><center><a href='http://bigclock.centro.ru/'>Вернуться на сайт</a>";
 
 
 }

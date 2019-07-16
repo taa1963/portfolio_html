682b56dacc2d73945c27e871bc690b283d5e25e6
taa1963 <taa1963@ukr.net>
2019-07-17 01:16:17 +0300

    14072019_3

---
+++ CalcGemo2/js/scriptsCalc.js
@@ -0,0 +1,233 @@
+
+$(document).ready(function() {
+
+
+    $('#calc_next').click(function () {
+        var Zr, Zk, Zm, Zkm,Zmm,Zrm, zv,zv2,zv3;
+        Zr = 0;
+        Zk = 0;
+        Zm = 0;
+        zv = 0;
+        Zkm=0;
+        Zmm=0;
+        Zrm=0;
+        zv2=0;
+        zv3=0;
+
+        var elems = document.getElementsByClassName('ekran')
+
+        for (var i = 0; i < elems.length; i++) {
+
+            var str = elems[i].id;
+            // alert(str);
+            zv = document.getElementById(str).value;
+            zv = +zv;
+
+            Zr = Zr + zv;
+            // $('#Zr').text(Zr);
+            // Zrm=Zr;
+            // $('#Zrm').text(Zrm);
+            // document.getElementById('ekran').value=Zrm;
+        }
+
+        $('#Zr').text(Zr);
+        Zrm=Zr;
+        $('#Zrm').text(Zrm);
+        document.getElementById('ekran').value=Zrm;
+         // alert(Zrm);
+        alert(Zrm);
+
+
+        var elems = document.getElementsByClassName('ekrank')
+
+        for (var i = 0; i < elems.length; i++) {
+
+            var str = elems[i].id;
+            // alert(str);
+             zv2 = document.getElementById(str).value;
+            if (str =="ekrank1"){
+                zv2 = +zv2;
+                zv2=zv2*2
+            }
+            if (str =="ekrank2"){
+                zv2 = +zv2;
+                zv2=zv2*3;
+                // alert(str);
+                // alert(zv2);
+
+            }
+
+            if (str =="ekrank3"){
+                zv2 = +zv2;
+                zv2=zv2*3;
+            }
+            if (str =="ekrank4"){
+                zv2 = +zv2;
+                zv2=zv2*4;
+            }
+
+
+        zv2 = +zv2;
+            // alert(zv3);
+            Zk = Zk + zv2;
+            // $('#Zk').text(Zk);
+            //  Zkm=Zk;
+            // $('#Zkm').text(Zkm);
+            // document.getElementById('ekrank').value=Zkm;
+
+        }
+        $('#Zk').text(Zk);
+        Zkm=Zk;
+        $('#Zkm').text(Zkm);
+        document.getElementById('ekrank').value=Zkm;
+
+
+
+
+        var elems = document.getElementsByClassName('ekranm')
+
+        for (var i = 0; i < elems.length; i++) {
+
+            var str = elems[i].id;
+            // alert(str);
+             zv3 = document.getElementById(str).value;
+            if (str =="ekranm1"){
+                zv3 = +zv3;
+                zv3=zv3*1;
+            }
+            if (str =="ekranm2"){
+                zv3 = +zv3;
+                zv3=zv3*1;
+            }
+
+            if (str =="ekranm3"){
+                zv3 = +zv3;
+                zv3=zv3*4;
+            }
+            if (str =="ekranm4"){
+                zv3 = +zv3;
+                zv3=zv3*2;
+            }
+            if (str =="ekranm5"){
+                zv3 = +zv3;
+                zv3=zv3*1;
+            }
+            if (str =="ekranm6"){
+                zv3 = +zv3;
+                zv3=zv3*1;
+            }
+            if (str =="ekranm7"){
+                zv3 = +zv3;
+                zv3=zv3*2;
+            }
+
+
+
+            zv3 = +zv3;
+            // alert(zv3);
+            Zm = Zm + zv3;
+            // $('#Zm').text(Zm);
+            // Zmm=Zm;
+            // $('#Zmm').text(Zmm);
+            // document.getElementById('ekranm').value=Zmm;
+
+        }
+        $('#Zm').text(Zm);
+        Zmm=Zm;
+        $('#Zmm').text(Zmm);
+        document.getElementById('ekranm').value=Zmm;
+
+
+
+    });
+
+
+    $('#calc_next').click(function () {
+        var block = $(this).parents('.calculator');
+        block.find('.calc_block_param').hide();
+        block.find('.calc_block_itog2').hide();
+        block.find('.calc_block_itog').show();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+
+    });
+
+    $('#calc_detail_old1').click(function () {
+        var block = $(this).parents('.calculator');
+        block.find('.calc_block_param').show();
+        block.find('.calc_block_itog2').hide();
+        block.find('.calc_block_itog').hide();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+        //scrollTop: $("#calc_block_param").offset().top;
+    });
+
+
+
+    $('#calc_detail').click(function () {
+        var block = $(this).parents('.calculator');
+        block.find('.calc_block_param').hide();
+        block.find('.calc_block_itog2').show();
+        block.find('.calc_block_itog').hide();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+        //scrollTop: $("#calc_block_itog2").offset().top;
+    });
+
+    $('#calc_detail_old2').click(function () {
+        var block = $(this).parents('.calculator');
+        block.find('.calc_block_param').hide();
+        block.find('.calc_block_itog2').hide();
+        block.find('.calc_block_itog').show();
+        //scrollTop: $("#calc_block_itog").offset().top;
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+
+    });
+
+
+    $('input').keydown(function(e) {
+
+        var divs = document.getElementsByName('dev');
+        if(e.keyCode === 13) {
+            var index = this.getAttribute('data-index');
+            index=+index;
+            index=index+1;
+            if (index==12){
+                index=11;
+            }
+            divs[index].focus();
+
+         }
+
+
+        var handler = function() {
+            // var index = this.dataset.index;
+            var index = this.getAttribute('data-index');
+            //alert(index);
+        }
+        for (var i = 0; i < divs.length; i++) {
+            // divs[i].dataset.index = i;
+            divs[i].setAttribute('data-index', i);
+            divs[i] = handler;
+        }
+    });
+
+
+    $('input').keypress(function(e) {
+        if (( e.which==8 ||e.which==44 ||e.which==45 ||e.which==46 )) return false;
+
+    });
+
+    $('.calc_block_param input').on('input change paste', function() {
+        $(this).val(this.value.replace(/[^0-9\-]/, '')); // запрещаем ввод любых символов, кроме цифр и знака минуса
+    });
+
+
+});
+
+
+
+
+
+
---
+++ CalcGemo2/send.php
@@ -0,0 +1,196 @@
+
+<?php
+// если была нажата кнопка "Отправить"
+if (
+    isset($_POST['ekran']) && !empty($_POST['ekran']) &&
+    isset($_POST['ekrank']) && !empty($_POST['ekrank']) &&
+    isset($_POST['ekranm']) && !empty($_POST['ekranm']) &&
+    isset($_POST['email']) && !empty($_POST['email'])) {
+    $ekran = substr(htmlspecialchars(trim($_POST['ekran'])), 0, 100);
+    $ekrank = substr(htmlspecialchars(trim($_POST['ekrank'])), 0, 100);
+    $ekranm = substr(htmlspecialchars(trim($_POST['ekranm'])), 0, 100);
+    $email = substr(htmlspecialchars(trim($_POST['email'])), 0, 1000000);
+    //$to = 'clublaser@mail.ru';
+    $to = $_POST['email'] ; ;
+    // $to = $email;
+    //$to = 'alexandr.tupichenkov@yandex.ru';
+    //$to = 'info@partwork.ru';
+    $title = 'ЧЕТВЕРТОЕ ПОЛЕ';
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .=  'Content-type: text/html;  charset=utf-8'. "\r\n";
+
+    $message = "
+       
+        <h1 style='text-align: center'> Добрый день! </h1>
+        <p style='text-align: left'>Опаснее всего для здоровья человека излучение от мобильного телефона, так как он находится в постоянной близости к телу. Поэтому, 
+        в первую очередь рекомендуется приобрести защиту на телефон \"Экран\".</p>
+        <p style='text-align: left'> При наличии роутера в квартире используйте защиту на роутер \"Экран-М\" - 1 шт.</p>
+        <p style='text-align: left'>Помимо этого для восстановления организма важно использовать аккумулятор-корректрор \"Витязь\" (достаточно 1 шт для всех членов семьи).  </p>
+        <p style='text-align: left'>Все остальные устройства рекомендуется защищать уже после этого. </p>
+        <p style='text-align: center'>*** </p>
+        <p style='text-align: left'>Вот количество защиты для часто используемых Вами устройств: </p>
+        
+
+          <table style='width: 50%;margin: 0 auto' class='table'  cellspacing='0'; border='1' >
+         
+                       <tr>
+                           <td style='text-align:center; width:70%; font-weight: bold'>Наименование устройства</td>
+
+
+                           <td style='text-align:center;width:30%'>Кол-во устройств </td>
+
+                       </tr>
+
+                       <tr>
+                           <th style='text-align:left'>Экран</th>
+                           <th style='text-align:left'>$ekran</th>
+                       </tr>
+
+                       <tr>
+                           <th style='text-align:left'>Экран-К</th>
+                           <th style='text-align:left'>$ekrank</th>
+                       </tr>
+
+                       <tr>
+                           <th style='text-align:left'>Экран-М</th>
+                           <th style='text-align:left'> $ekranm</th>
+                       </tr>
+                   </table>
+                   
+                  <p style='text-align: left'> Здесь подробная информация, куда и в каком количестве устанавливать защиту. </p> 
+        
+        <p><label>***</label></p>
+                     <table style='width: 90%;margin: 0 auto' class='table'  cellspacing='0'; border='1' >
+
+                         <tr>
+                             <td style='text-align:center; width:30%; font-weight: bold'>Куда</td>
+                             <td style='text-align:center;width:50%'>Как </td>
+                             <td style='text-align:center;width:10%'>Что </td>
+                             <td style='text-align:center;width:10%'>Кол-во </td>
+
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на мобильный телефон</th>
+                             <th style='text-align:center'>Устанавливается под крышкой мобильного телефона аккумуляторного отсека цветным знаком к крышке или в чехле телефона</th>
+                             <th style='text-align:center'>Экран</th>
+                             <th style='text-align:center'>1</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на планшет</th>
+                             <th style='text-align:center'>две пластины с обратной стороны параллельно коротким граням на одинаковом расстоянии от краев и друг друга</th>
+                             <th style='text-align:center'>Экран-К</th>
+                             <th style='text-align:center'>2</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на ноутбук</th>
+                             <th style='text-align:center'>пластины — по краям монитора по диагонали, одна с обратной стороны основной части ноутбука по центру</th>
+                             <th style='text-align:center'>Экран-К</th>
+                             <th style='text-align:center'>3</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на персональный компьютер</th>
+                             <th style='text-align:center'>пластины — по бокам монитора на каждую сторону по одной, одна — на процессор, обращенный в сторону оператора</th>
+                             <th style='text-align:center'>Экран-К</th>
+                             <th style='text-align:center'>3</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на телевизор</th>
+                             <th style='text-align:center'>Сзади экрана по 1 шт по углам</th>
+                             <th style='text-align:center'>Экран-К</th>
+                             <th style='text-align:center'>4</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на роутер</th>
+                             <th style='text-align:center'>пластина — на центральную часть верхней поверхности роутера</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>1</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на декодер для цифрового телевидения</th>
+                             <th style='text-align:center'>пластина — на центральную часть верхней поверхности декодера</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>1</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на микроволновую печь</th>
+                             <th style='text-align:center'>четыре пластины — по боковым граням СВЧ на каждую сторону по одной</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>4</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на кондиционер</th>
+                             <th style='text-align:center'>в центре по боковым торцам кондиционера.</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>2</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>в машину</th>
+                             <th style='text-align:center'>пластина — на центральную консоль под мультимедийной системой машины</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>1</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на видеорегистратор в машине</th>
+                             <th style='text-align:center'>пластина — на видеорегистратор на боковую грань, обращенную в сторону салона</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>1</th>
+                         </tr>
+
+                         <tr>
+                             <th style='text-align:left'>на систему охраны с видеонаблюдением/ видеодомофон</th>
+                             <th style='text-align:center'>в центре на верхней и нижней грани корпуса видеонаблюдения.</th>
+                             <th style='text-align:center'>Экран-М</th>
+                             <th style='text-align:center'>2</th>
+                         </tr>
+
+
+
+                     </table>
+        
+        
+        <p style='text-align: left'>Защитные устройства можно приобрести в интернет-магазине на сайте 4pole.ru.</p>
+        
+           
+       ";
+
+
+    $verify = mail($to, $title, $message, $headers);
+    ini_set('display_errors','On');
+    error_reporting('E_ALL');
+    if ($verify) {
+
+        header('Location: https://aleksandr.tupichenkov.com/CalcGemo/#popup_success', true, 302);
+        exit;
+    } else {
+        header('Location: https://aleksandr.tupichenkov.com/CalcGemo/#popup_success', true, 302);
+        exit;
+    }
+}
+
+
+//    if ($verify) {
+//        echo '<head>
+//    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+//    <body><center><font color="green"> Заявка отправлена.</font><br/>
+//   <center><a href="https://aleksandr.tupichenkov.com/CalcGemo">Вернуться на главную</a>
+//   </body>';}
+//    else {
+//        echo '<head>
+//    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+//    <body><center>Письмо не отправлено.<br/> <center>
+//   <a href="https://aleksandr.tupichenkov.com/CalcGemo">Вернуться на главную</a>
+//   </body>';}
+//}
+
+?>

8a3059b1877c24f018d141cbde9c31184c9121d4
taa1963 <taa1963@ukr.net>
2021-07-22 22:26:26 +0300

    22072021_1

---
+++ CalcAvto/js/scriptcalc.js
@@ -0,0 +1,205 @@
+var  ZenaZRL, ZenakgeotekL,ZenaShebL,ZenaKgidroisolL,Zenakarmrfrk24L,ZenaZkmontagopalL,ZenaBetonVibrL,ZenaZacuprL,ZenaVunosL,ZenaGeomagL,ZenaPesokSL,ZenaPesokNSL,ZenaShebenSL,ZenaShebenNSL,
+ZenaGeomemL,ZenaDoska40L,ZenaPpal70L,ZenaPpal71L,ZenaFanera18L,ZenaFiksatorL,ZenaArm12L,ZenaArm8L,ZenaProvL,ZenaBeton22L,ZenaDostavArmL,ZenaDostavOpalL,ZenaDostavMaterL,ZenaDostavInstrL,
+ZenaGruntL,ZenaZakladVL,ZenaZakladEL,ZenaZakladKanalL,ZenaDrenagL,ZenaMusorL,ZenaBetonNasosL,ZenaVagonL,ZenaBiotualL,ZenaElektroL,ZenaVaterL,ZenaZRPlita,ZenkgeotekPlita,ZenaShebPlita,ZenaPesokPlita,
+ZenaKgidroisolPlita,ZenaZkmontagopalPlita,Zenakarmrfrk24Plita,ZenaBetonVibrPlita,ZenaVunosPlita,ZenaGeomagPlita,ZenaPesokSPlita,ZenaPesokNSPlita,ZenaShebenSPlita,ZenaShebenNSPlita,ZenaGeomemPlita,
+ZenaDoska40Plita,ZenaArmatPlita,ZenaArm12Plita,ZenaArm8Plita,ZenaProvPlita,ZenaBeton22Plita,ZenaDostavArmPlita,ZenaDostavOpalPlita,ZenaDostavMaterPlita,ZenaDostavInstrPlita,ZenaZakladVPlita,ZenaZakladEPlita,
+ZenaZakladKanalPlita,ZenaDrenagPlita,ZenaMusorPlita,ZenaBetonNasosPlita,ZenaVagonPlita,ZenaBiotualPlita,ZenaElektroPlita,ZenaVaterPlita,ZenaGruntPlita,
+ZenaZRPlita,ZenkgeotekPlita,ZenaShebPlita,ZenaPesokPlita,
+ZenaZRPlitaNR,ZenaKgidroisolPlitaNR,ZenaZkmontagopalPlitaNR,Zenakarmrfrk24PlitaNR,ZenaBetonVibrPlitaNR,ZenaVunosPlitaNR,ZenaGeomagPlitaNR,ZenaPesokSPlitaNR,ZenaPesokNSPlitaNR,
+ZenaShebenSPlitaNR,ZenaShebenNSPlitaNR,ZenaGeomemPlitaNR,ZenaDoska40PlitaNR,ZenaArmatPlitaNR,ZenaArm12PlitaNR,ZenaArm8PlitaNR,ZenaProvPlitaNR,ZenaBeton22PlitaNR,ZenaDostavArmPlitaNR,
+ZenaDostavOpalPlitaNR,ZenaDostavMaterPlitaNR,ZenaDostavInstrPlitaNR,ZenaZakladVPlitaNR,ZenaZakladEPlitaNR,ZenaZakladKanalPlitaNR,ZenaDrenagPlitaNR,ZenaMusorPlitaNR,
+ZenaBetonNasosPlitaNR,ZenaVagonPlitaNR,ZenaBiotualPlitaNR,ZenaElektroPlitaNR,ZenaVaterPlitaNR,ZenaGruntPlitaNR,ZenaGranPlitaNR,ZenaZkmontagopalPlitaRosNR,ZenaBetonPlitaRosNR,ZenaOpalFaneraPlitaNR,
+ZenaZRPlitaVR,ZenaKgidroisolPlitaVR,ZenaZkmontagopalPlitaVR,Zenakarmrfrk24PlitaVR,ZenaBetonVibrPlitaVR,ZenaVunosPlitaVR,ZenaGeomagPlitaVR,ZenaPesokSPlitaVR,ZenaPesokNSPlitaVR,
+ZenaShebenSPlitaVR,ZenaShebenNSPlitaVR,ZenaGeomemPlitaVR,ZenaDoska40PlitaVR,ZenaArmatPlitaVR,ZenaArm12PlitaVR,ZenaArm8PlitaVR,ZenaProvPlitaVR,ZenaBeton22PlitaVR,ZenaDostavArmPlitaVR,
+ZenaDostavOpalPlitaVR,ZenaDostavMaterPlitaVR,ZenaDostavInstrPlitaVR,ZenaZakladVPlitaVR,ZenaZakladEPlitaVR,ZenaZakladKanalPlitaVR,ZenaDrenagPlitaVR,ZenaMusorPlitaVR,
+ZenaBetonNasosPlitaVR,ZenaVagonPlitaVR,ZenaBiotualPlitaVR,ZenaElektroPlitaVR,ZenaVaterPlitaVR,ZenaGruntPlitaVR,ZenaGranPlitaVR,ZenaZkmontagopalPlitaRosVR,ZenaBetonPlitaRosVR,ZenaOpalFaneraPlitaVR,
+ZenaOSUSP,ZenaZRUSP,ZenaGrUSP,ZenkgeotekUSP,ZenaDrenagUSP,ZenaPesokUSP,ZenaVvodVaterUSP,ZenaKanalUSP,ZenaDrenagUSP,ZenaPsbUSP,ZenaOpalUSP,ZenaArmUSP,ZenaPolUSP,ZenaBetonUSP,ZenaOtmosUSP,
+ZenaZatirUSP,ZenaGeomagUSP,ZenaPesokSUSP,ZenaPesokNSUSP,ZenaShebenSUSP,ZenaShebenNSUSP,ZenaTubaDKSUSP,ZenaFitingUSP,ZenaDrenag2USP,ZenaLukUSP,ZenaTrubaDrUSP,ZenaTehnoUSP,ZenaPCB35USP,
+ZenaPenoplUSP,ZenaPlenkaUSP,ZenaPenaUSP,ZenaTubaUponorUSP,ZenaUgolUSP,ZenaStjqUSP,ZenaEnergofUSP,ZenaDoska40USP,ZenaFiting2USP,ZenaKollektorUSP,ZenaKranUSP,ZenaAntifrisUSP,ZenaStArmUSP,
+ZenaArm12USP,ZenaProvolUSP,ZenaBeton2USP,ZenaSetkaUSP,ZenaDostavkaPiloUSP,ZenaDostavkaArmUSP,ZenaDostavkaDrUSP,ZenaDostavkaInstrUSP,ZenaMusorUSP,ZenaBetonNasosUSP,ZenaVagonUSP,ZenaBiotuaUSP,
+ZenaElektroUSP,ZenaVaterUSP,ZenaSatirAvtoUSP,
+ZenaOSZOK,ZenaZRZOK,ZenkgeotekZOK,ZenaPesokZOK,ZenaSbenkZOK,ZenaOpalZOK,ZenaArmZOK,ZenaBetonZOK,ZenaOS2ZOK,ZenaArmKaskZOK,ZenaOpalZZOK,ZenaOpalGorZOK,ZenaOpalVertZOK,ZenaGeomagZOK,
+ZenaPesokSZOK,ZenaPesokNSZOK,ZenaShebenSZOK,ZenaShebenNSZOK,ZenaDoska40ZOK,ZenaStArmZOK,ZenaArm12ZOK,ZenaProvolZOK,ZenaBeton2ZOK,ZenaFiksatorZOK,ZenaOpalInvZOK,ZenaArm8ZOK,
+ZenaOpalInv2ZOK,ZenaFaneraZOK,ZenaDostavkaArmZOK,ZenaDostavkaOpalZOK,ZenaDostavkaDrZOK,ZenaDostavkaInstrZOK,ZenaGruntPlanZOK,ZenaPesokPas88SZOK,ZenaPesokPas88NSZOK,ZenaZaklGilsWZOK,
+ZenaZaklGilsEZOK,ZenaKanalDZOK,ZenaBeton100ZOK,ZenaGidroPodZOK,ZenaGidroZokZOK,ZenaTeploZokZOK,ZenaGidroShZOK,ZenaMusorZOK,ZenaBetonNasosZOK,ZenaVagonZOK,ZenaKranZOK,ZenaBiotuaZOK,
+ZenaElektroZOK,ZenaVateZOK,
+VarFund,zemv,zenzemv,zena_smetu, VarMail, VarSM, zena_mail, ZenaBeton, ZenaDoska, ZenaArmatur,
+    zkpesok, Sezon;
+
+VarFund=1;
+zemv=0;
+zenzemv=0;
+zena_smetu=0;
+zena_mail=0;
+VarMail=0;
+VarSM=1;
+
+Sezon=1;
+//========== Сезонные работы =================//
+//Sezon=1;-Зима    Sezon=0;-Лето//
+
+
+
+$(document).ready(function() {
+
+
+
+
+
+    var url = "https://aleksandr.tupichenkov.com/CalcFund6/js/price.xlsx";
+        // alert(url);
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", url, true);
+        oReq.responseType = "arraybuffer";
+
+        oReq.onload = function(e) {
+            var arraybuffer = oReq.response;
+            // convert data to binary string //
+            var data = new Uint8Array(arraybuffer);
+
+            var arr = new Array();
+            for (var i = 0; i != data.length; ++i) {
+                arr[i] = String.fromCharCode(data[i]);
+            }
+            console.log(arr);
+
+            var bstr = arr.join("");
+            console.log(bstr);
+            var cfb = XLSX.read(bstr, { type: 'binary' });
+
+            //=======================================//
+            var workbook = XLSX.read(bstr, {type:"binary"});
+            var first_sheet_name = workbook.SheetNames[0];
+            / Get worksheet /
+            var worksheet = workbook.Sheets[first_sheet_name];
+            //  console.log(worksheet);
+
+            var address_of_cell = 'B3';
+            var desired_cell = worksheet[address_of_cell];
+            ZenaZRL = desired_cell.v;
+
+
+
+
+        }
+
+        oReq.send();
+
+
+
+
+
+
+//========== Выберите марку авто =======//
+    $('#typeavto').change(function () {
+        var tavto=document.getElementById('typeavto').value;
+        if (tavto==0){
+            $('.block_calc_name').removeClass('active');
+            $('.calc_block_itog').removeClass('active2');
+        }
+        else{
+
+            Zena();
+        }
+
+
+        Zena();
+    });
+
+    //========== Выберите Вид работ =======//
+    $('#typearb').change(function () {
+
+        var tarb=document.getElementById('typearb').value;
+        if (tarb==0){
+            $('.block_calc_name').removeClass('active');
+        }
+        else{
+            $('.block_calc_name').addClass('active');
+            Zena();
+        }
+
+
+    });
+
+    //========== Выберите Вид работ =======//
+    $('#namearb').change(function () {
+
+        var Narb=document.getElementById('namearb').value;
+        if (Narb==0){
+            $('.calc_block_itog').removeClass('active2');
+        }
+        else{
+            $('.calc_block_itog').addClass('active2');
+            Zena();
+        }
+
+
+    });
+
+
+
+    //========== Функция расчета цены=======//
+
+
+    function Zena() {
+
+        var Zena=0;
+
+        var typeavtod=$("#typeavto option:selected").html();
+        $('#typеavtoi').text(typeavtod);
+        document.getElementById('typem').value=typeavtod;
+
+        var typearbd=$("#typearb option:selected").html();
+        $('#vidarbi').text(typearbd);
+        document.getElementById('vidarbm').value=typearbd;
+
+        var namearbd=$("#namearb option:selected").html();
+        $('#namearbi').text(namearbd);
+        document.getElementById('namearbm').value=namearbd;
+
+
+        var ZKia=$("#namearb option:selected").attr('data-kia');
+        var ZMylt=$("#namearb option:selected").attr('data-mylt');
+        var ZVesta=$("#namearb option:selected").attr('data-vesta');
+        var ZGranta=$("#namearb option:selected").attr('data-granta');
+
+
+
+        console.log(ZKia)
+        console.log(ZGranta)
+        console.log(ZMylt)
+        console.log(ZVesta)
+
+
+        var Tavto=document.getElementById('typeavto').value;
+
+        console.log(Tavto)
+
+        if(Tavto==0){
+            Zena=0
+        }
+        if(Tavto==1){
+            Zena=ZKia;
+        }
+
+        if ((Tavto==5)||(Tavto==6)){
+            Zena=ZGranta;
+        }
+
+
+        if ((Tavto==2)||(Tavto==3)||(Tavto==4)){
+            Zena=ZVesta;
+        }
+        if(Tavto==7){
+            Zena=ZMylt;
+        }
+
+        $('#zenaarbi').text(Zena);
+
+        document.getElementById('zenaarbm').value=Zena;
+
+
+    };
+
+
+});

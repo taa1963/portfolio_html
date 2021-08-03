01e33b3ef4ee48ad604fb0b8b6b7d4c937a656f7
taa1963 <taa1963@ukr.net>
2021-08-03 23:40:20 +0300

    3082021_5

--- CalcSetka5/send2.php
+++ CalcSetka5/send2.php
@@ -13,24 +13,24 @@ $items = $_POST['item'];
 if (
     isset($_POST['name2']) && !empty($_POST['name2']) &&
     isset($_POST['tel2']) && !empty($_POST['tel2']) &&
-    isset($_POST['num_mail']) && !is_int($_POST['num_mail']) &&
-    isset($_POST['rasmer_mail']) && !is_int($_POST['rasmer_mail']) &&
-    isset($_POST['typepol_mail']) && !is_int($_POST['typepol_mail']) &&
-    isset($_POST['colorpol_mail']) && is_int($_POST['colorpol_mail']) &&
-    isset($_POST['devpol_mail']) && is_int($_POST['devpol_mail']) &&
-    isset($_POST['zena_mail']) && is_int($_POST['zena_mail']) &&
+    //isset($_POST['num_mail']) && !is_int($_POST['num_mail']) &&
+   // isset($_POST['rasmer_mail']) && !is_int($_POST['rasmer_mail']) &&
+   // isset($_POST['typepol_mail']) && !is_int($_POST['typepol_mail']) &&
+    //isset($_POST['colorpol_mail']) && is_int($_POST['colorpol_mail']) &&
+    //isset($_POST['devpol_mail']) && is_int($_POST['devpol_mail']) &&
+   // isset($_POST['zena_mail']) && is_int($_POST['zena_mail']) &&
     //isset($_POST['dostavka_mail']) && is_int($_POST['dostavka_mail']) &&
     isset($_POST['itogzena_mail']) && is_int($_POST['itogzena_mail']))
 	{
 
     $name2 = substr(htmlspecialchars(trim($_POST['name2'])), 0, 1000);
     $tel2 = substr(htmlspecialchars(trim($_POST['tel2'])), 0, 1000);
-    $num_mail = substr(htmlspecialchars(trim($_POST['num_mail'])), 0, 1000);
-    $rasmer_mail = substr(htmlspecialchars(trim($_POST['rasmer_mail'])), 0, 100);
-    $typepol_mail = substr(htmlspecialchars(trim($_POST['typepol_mail'])), 0, 10000000000);
-    $colorpol_mail = substr(htmlspecialchars(trim($_POST['colorpol_mail'])), 0, 10000000000);
-    $devpol_mail = substr(htmlspecialchars(trim($_POST['devpol_mail'])), 0, 10000000000);
-    $zena_mail = substr(htmlspecialchars(trim($_POST['zena_mail'])), 0, 10000000000);
+  //  $num_mail = substr(htmlspecialchars(trim($_POST['num_mail'])), 0, 1000);
+  //  $rasmer_mail = substr(htmlspecialchars(trim($_POST['rasmer_mail'])), 0, 100);
+  //  $typepol_mail = substr(htmlspecialchars(trim($_POST['typepol_mail'])), 0, 10000000000);
+  //  $colorpol_mail = substr(htmlspecialchars(trim($_POST['colorpol_mail'])), 0, 10000000000);
+  //  $devpol_mail = substr(htmlspecialchars(trim($_POST['devpol_mail'])), 0, 10000000000);
+  //  $zena_mail = substr(htmlspecialchars(trim($_POST['zena_mail'])), 0, 10000000000);
     //$dostavka_mail = substr(htmlspecialchars(trim($_POST['dostavka_mail'])), 0, 10000000000);
     $itogzena_mail = substr(htmlspecialchars(trim($_POST['itogzena_mail'])), 0, 10000000000);
 
@@ -45,12 +45,7 @@ if (
         $message = "
          Был получен заказ с сайта москитных сеток:
 
-        Количество сеток: $num_mail
-        Размер сеток: $rasmer_mail
-        Тип полотна: $typepol_mail
-        Цвет полотна:$colorpol_mail
-        Тип крепежа:$devpol_mail
-        Цена сетки:$zena_mail
+       
         Доп параметры
         Итоговая цена:$itogzena_mail
         От клиента:

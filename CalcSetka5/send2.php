2dedfb018474043a873d9ca5e01f918995837c1a
taa1963 <taa1963@ukr.net>
2021-08-03 21:58:51 +0300

    3082021_3

--- CalcSetka5/send2.php
+++ CalcSetka5/send2.php
@@ -1,10 +1,10 @@
 <?php
 // если была нажата кнопка "Отправить"
-//print_r($_POST);
+print_r($_POST);
 
 //print_r(iconv("UTF-8", "UTF-8", $_POST));
 
-//$items = $_POST['item'];
+$items = $_POST['item'];
 
 //$result = '';
 
@@ -19,7 +19,7 @@ if (
     isset($_POST['colorpol_mail']) && is_int($_POST['colorpol_mail']) &&
     isset($_POST['devpol_mail']) && is_int($_POST['devpol_mail']) &&
     isset($_POST['zena_mail']) && is_int($_POST['zena_mail']) &&
-    isset($_POST['dostavka_mail']) && is_int($_POST['dostavka_mail']) &&
+    //isset($_POST['dostavka_mail']) && is_int($_POST['dostavka_mail']) &&
     isset($_POST['itogzena_mail']) && is_int($_POST['itogzena_mail']))
 	{
 
@@ -31,7 +31,7 @@ if (
     $colorpol_mail = substr(htmlspecialchars(trim($_POST['colorpol_mail'])), 0, 10000000000);
     $devpol_mail = substr(htmlspecialchars(trim($_POST['devpol_mail'])), 0, 10000000000);
     $zena_mail = substr(htmlspecialchars(trim($_POST['zena_mail'])), 0, 10000000000);
-    $dostavka_mail = substr(htmlspecialchars(trim($_POST['dostavka_mail'])), 0, 10000000000);
+    //$dostavka_mail = substr(htmlspecialchars(trim($_POST['dostavka_mail'])), 0, 10000000000);
     $itogzena_mail = substr(htmlspecialchars(trim($_POST['itogzena_mail'])), 0, 10000000000);
 
 
@@ -52,7 +52,6 @@ if (
         Тип крепежа:$devpol_mail
         Цена сетки:$zena_mail
         Доп параметры
-        Цена доставки:$dostavka_mail
         Итоговая цена:$itogzena_mail
         От клиента:
         Имя:$name2

7aae016b4fc9d76c847b9a29c66d197d99c4cade
taa1963 <taa1963@ukr.net>
2023-07-10 23:30:01 +0300

    10072023_4

---
+++ CalcFundNewSPBAdmin4/js/ajax.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+
+  
+
+
+    $("#but_block_adm_3").click(function(){
+        var ZbutovkaAdmv=document.getElementById('ZbutovkaAdm').value;
+
+        var strvv='';
+        strvv=strvv+'//======== Бытовка металлическая========//'+"\n";
+        strvv=strvv+'var Zbutovka='+' '+ZbutovkaAdmv+';'+'//Цена бытовки'+"\n" ;
+        // strvv=strvv+'var Zbutovkapr='
+
+
+         $('#stsx').text(strvv);
+         var block = $(this).parents('.zakas');
+        block.find('.zakas_block_admin2').show();
+        block.find('.zakas_block_admin1').hide();
+        block.find('.zakas_block1').hide();
+        block.find('.zakas_block2').hide();
+        block.find('.zakas_block3').hide();
+        block.find('.zakas_block3').hide();
+        //alert(2)
+
+
+
+
+           sendAjaxForm('result_form', 'form1', 'saves.php');
+           return false;
+        }
+    );
+});
+
+
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url: url,
+
+        type:     "post", //метод отправки
+        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        dataType: "html", //формат данных
+        success: function(response) { //Данные отправлены успешно
+           // console.log(response);
+            // отправляем данные в ГТМ
+			//dataLayer.push({'event':'formSendCalk'});
+            if (response.status !== 'ok') {
+                //$('.plankal2').addClass('plankaadd');
+                
+
+                
+                alert('Данные успешно отправлены. Мы Вам перезвоним в ближайшее время.');
+                return;
+            }
+
+           		 
+			 
+        },
+        error: function(response) { // Данные не отправлены
+
+           
+         
+            alert('Данные yyyyyy  успешно отправлены. Мы Вам перезвоним в ближайшее время.');
+        }
+    });
+}
---
+++ CalcFundNewSPBAdmin4/saves.php
@@ -0,0 +1,94 @@
+<?php
+// если была нажата кнопка "Отправить"
+//print_r($_POST);
+
+//print_r(iconv("UTF-8", "UTF-8", $_POST));
+
+//$items = $_POST['item'];
+
+//$result = '';
+
+
+//echo $result;
+if (
+    isset($_POST['stsx']) && !empty($_POST['stsx']) 
+   ) 
+	{
+    $stsx = substr(htmlspecialchars(trim($_POST['stsx'])), 0, 100000000000000);
+	
+	
+
+    // на какие данные рассчитан этот скрипт
+     header("Content-Type: application/hmml");
+	 
+	 unlink('data2.js');
+
+     // Берём новую переменную и пишем в неё имя файла
+    // $filename = 'data2.js';
+	 
+
+
+     // 3. Если есть — запоминаем его содержимое, а если такого файла нет — создаём его отдельной командой.
+if (file_exists($filename)) {
+    // Если файл есть — открываем его и читаем данные
+    $file = file_get_contents('data2.js');
+    // Если такого файла нет…
+} 
+else 
+{
+// …то создаём его сами
+    $file = fopen("data2.js", "a+");
+	 if ($f === false) {
+        return 0;
+		 echo 'не открыли';
+    } else {
+       echo 'Открыли  успешно';
+    }
+}
+	
+    //$fh = fopen('data2.js', 'a');
+    //fwrite($fh, '<h1>Привет, мир!</h1>');
+    file_put_contents($file, $stsx);
+	
+	$fcontents = file_get_contents($file, $stsx);)
+      
+      echo $fcontents
+	 
+	
+	
+	
+	$file2 = fopen("data.js", "a+");
+    
+	$current .= "Test_John Smith\n";
+     // Пишем содержимое обратно в файл
+   file_put_contents($file2, $current);
+   
+   echo 'Изменение модификации файла прошло успешно';
+
+    fclose($file);
+	fclose($file2);
+
+
+
+
+
+    // 4/ Всё, что было в этом файле, переводим в массив, с которым умеет работать PHP. Таким способом у нас каждая JSON-запись будет храниться в отдельной ячейке массива.
+//$taskList = '';
+// 5. Добавляем новую запись в этот массив — кладём в него то, что пришло со страницы.
+//$taskList = $stsx;
+// 6. Записываем это всё обратно в файл и тут же читаем обратно из него — так мы убедимся, что запись прошла нормально и мы можем с этим работать.
+// Записываем данные в файл…
+//file_put_contents('data2.js', json_encode($taskList));
+
+
+// Освобождаем память от переменных, которые нам пока не нужны
+//unset($file);
+//unset($taskList);
+
+    
+
+
+    
+    }
+
+?>

4fb0bb4bb4c23628effb750a26fc6ff8e8d51ce2
taa1963 <taa1963@ukr.net>
2023-07-11 16:39:09 +0300

    11072023_8_1

---
+++ CalcFundNewSPBAdmin8/saves.php
@@ -0,0 +1,27 @@
+<?php
+
+if (
+    isset($_POST['stsx']) && !empty($_POST['stsx'])
+) {
+    $stsx = substr(htmlspecialchars(trim($_POST['stsx'])), 0, 100000000000000);
+
+    header("Content-Type: application/html");
+
+// Берём новую переменную и пишем в неё имя файла
+    $filename = 'data2.js';
+    $fullFilePath = _DIR_ . DIRECTORY_SEPARATOR . $filename;
+
+// Выводим на экран то куда сохраняем
+    echo $fullFilePath . "\n";
+
+// Сохраняем
+    file_put_contents($fullFilePath, $stsx);
+
+    $fcontents = file_get_contents($fullFilePath, $stsx);
+
+// Выводим на екран контент файла
+    echo $fcontents;
+}
+
+
+?>
